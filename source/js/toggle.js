3b83eacc2d23550e1420805e79ff916d9802090f
Aaron <yewchung_1995@hotmail.com>
2020-09-05 21:02:31 +0800

    Draft Version 1.0
    FInish all the requirements but still need to make it responsive.

---
+++ source/js/toggle.js
@@ -0,0 +1,8 @@
+function toggleDisclaimer() {
+  var x = document.getElementById("disclaimer-references");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
